d438181dae787d691ad7c347e5ce5e8e594f6883
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-11-16 20:48:55 -0500

    primer commit

---
+++ Semana 1/Dia 1/01-variables.py
@@ -0,0 +1,12 @@
+# Para definir variables numericas
+numero = 1
+numerodecimal = 18.5
+
+# Variables de tipo texto
+texto = 'Soy un texto'
+otrotexto= "Soy otro texto"
+
+# Para saber que tipo de variable es
+print(type(numerodecimal))
+# Para mostrar algo en la consola print()
+print(1,2,3)
