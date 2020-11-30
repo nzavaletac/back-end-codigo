b8ba548ea700a63b2cb5053572c1f82293f303e2
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-11-30 18:56:53 -0500

    avance

--- Semana 2/Dia 4/01-flaskRestful.py
+++ Semana 2/Dia 4/01-flaskRestful.py
@@ -1,8 +1,11 @@
 # pip3 install flask-restful
+# pip3 install flask-cors
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 items =[{
     'prodNomb': 'Sapolio',
     'prodPrec':5.40,
@@ -41,15 +44,34 @@ def buscar():
         'content': resultado
     }
 
-
-
 class Item(Resource):
+    # https://flask-restful.readthedocs.io/en/latest/reqparse.html
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'prodNom',
+        type=str,
+        required=True,
+        help='Falta el nombre del Producto'
+    )
+    parser.add_argument(
+        'prodPrec',
+        type=float,
+        required=True,
+        help='Falta el precio del Producto'
+    )
+    parser.add_argument(
+        'prodCar',
+        type=list, 
+        required=True,
+        location='json',
+        help='Falta las caracteristicas del Producto'
+    )
     def get(self, id):
-        if len(item) > id:
+        if len(items) > id:
             return {
                 'ok': True,
                 'message':None,
-                'content': item[id]
+                'content': items[id]
             }
         else:
             return {
@@ -58,7 +80,43 @@ class Item(Resource):
                 'content': None
             }
     def post(self):
-        return 'ok'
+        data = self.parser.parse_args()
+        print(data)
+        items.append(data)
+        return {
+            'ok': True,
+            'message': 'Se agrego exitosamente el item',
+            'content': data
+        }
+    def put(self,id):
+        if len(items) > id:
+            data = self.parser.parse_args()
+            items[id] = data
+            return {
+                'ok':True,
+                'message':'Se actualizo el item',
+                'content': data
+            }, 201
+        else:
+            return {
+                'ok': False,
+                'message': 'No se encontro el item a actualizar',
+                'content': None
+            }, 404
+    def delete(self, id):
+        if len(items) > id:
+            items.pop(id)
+            return {
+                'ok': True,
+                'message': 'Se elimino el item',
+                'content': None
+            }
+        else:
+            return {
+                'ok': False,
+                'message': 'No se encontro el item a eliminar',
+                'content': None
+            }, 404
 
 # Con el uso de flask_restfill ya no se necesitan decoradores , solamente se pasa un parametro para agregar un recurso a la api
 api.add_resource(Item,'/item','/item/<int:id>')
