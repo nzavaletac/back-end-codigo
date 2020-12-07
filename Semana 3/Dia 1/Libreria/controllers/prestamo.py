1e0937be02aab3431461158c64d9c56ce13b9c64
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-12-07 18:42:40 -0500

    avance

--- Semana 3/Dia 1/Libreria/controllers/prestamo.py
+++ Semana 3/Dia 1/Libreria/controllers/prestamo.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from models.prestamo import PrestamoModel
 from models.cliente import ClienteModel
+from datetime import datetime
 
 class PrestamosController(Resource):
     def get(self):
@@ -53,11 +54,27 @@ class PrestamosController(Resource):
         # 1. que verifique que el usuario no tenga algun libro pendiente de devolucion 
         # 2. vea si existen ejemplares de ese libro a prestar (o sea que los prestamos actuales de ese libro no superen a la cantidad del mismo)
         # 3. que al momento de realizar el prestamo el libro y el cliente esten con estado True
-        prestamo = PrestamoModel(resultado['fecha_inicio'], resultado['fecha_fin'], resultado['cliente'], resultado['libro'])
-        prestamo.save()
+        fechaActual = datetime.date(datetime.now())
+        prestamoCliente = PrestamoModel.query.filter_by(cliente=resultado['cliente']).all()
+        # SELECT * FROM T_PRESTAMO WHERE CLI_ID = RESULTADO['CLIENTE']
+        print(prestamoCliente)
+        if prestamoCliente:
+            # la persona tiene un historial de prestamos
+            restriccion = 0 # es una bandera (flag)
+            for prestamo in prestamoCliente:
+                print(prestamo.fechfin_prestamo)
+                if prestamo.fechfin_prestamo > fechaActual:
+                    # la fecha de devolucion es mayor que el dia actual por ende hay un prestamo que aun no devuelve el libro
+                    restriccion += 1 # restriccion = restriccion + 1
+            
+        else:
+            # la persona nunca ha realizado un prestamo
+            pass
+        # prestamo = PrestamoModel(resultado['fecha_inicio'], resultado['fecha_fin'], resultado['cliente'], resultado['libro'])
+        # prestamo.save()
+        print(restriccion)
         return {
             'ok':True,
-            'content':prestamo.devolverJson(),
+            # 'content':prestamo.devolverJson(),
             'message': 'Prestamo creado exitosamente'
         },201
-        
--- Semana 3/Dia 1/Libreria/models/prestamo.py
+++ Semana 3/Dia 1/Libreria/models/prestamo.py
@@ -5,6 +5,7 @@ class PrestamoModel(bd.Model):
                             primary_key=True, autoincrement=True, nullable=False)
     fechin_prestamo = bd.Column('prestamo_fechin', bd.Date, nullable=False)
     fechfin_prestamo = bd.Column('prestamo_fechfin', bd.Date)
+    fechentrega_prestamo = bd.Column('prestamo_fechentrega', bd.Date, nullable=True)
     estado = bd.Column(bd.Boolean, default=True, nullable=False)
     # RELACIONES
     cliente = bd.Column('cli_id', bd.Integer, bd.ForeignKey('t_cliente.cli_id'), nullable=False)
