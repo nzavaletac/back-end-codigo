eb3d4a8be6ea5bd3dc5eca79d3575cfb0af73a1f
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-12-08 20:42:02 -0500

    avance

--- Semana 4/Dia 2/app.py
+++ Semana 4/Dia 2/app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from bd import bd
+from models.usuario import UsuarioModel
+from models.contacto import ContactoModel
+from models.proyecto import ProyectoModel
+from models.redsocial import RedSocialModel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@localhost:3306/portafolioflask'
 # para pasar variables de mi funcion a mi html uso en el html {{variable}}
 # para usar statements como por ejemplo un for, un bloque u otros se usa 
 # {% template_tag %}....
 # {% fin_template_tag %}
+
+@app.before_first_request
+def creacion_tablas():
+    bd.init_app(app)
+    bd.create_all(app=app)
 
 @app.route('/')
 def pagina_principal():
@@ -15,7 +26,8 @@ def pagina_principal():
 
 @app.route('/proyectos')
 def proyectos():
-    return render_template('proyectos.html')
+    mis_proyectos = ['Proyecto1', 'Proyecto2', 'Proyecto3', 'Proyecto4']
+    return render_template('proyectos.html', proyectos=mis_proyectos)
 
 @app.route('/contact')
 def contactamen():
