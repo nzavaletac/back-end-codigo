ceea78651228bae0d47ff3e5f056f949e00e22fa
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-01-04 21:46:13 -0500

    crud al 80%

--- Semana 6/Tareas/src/controllers/TareaController.js
+++ Semana 6/Tareas/src/controllers/TareaController.js
@@ -1,14 +1,61 @@
 // Aquí se va a definir todos los controladores (comportamiento que va a recibir cuando se llama a una ruta determinada con su metodo de acceso (get, post, put, delete))
-let tareas = [];
+let tareas = [
+    {
+        nombre: "Ir al gym",
+        importancia: "Baja"
+    }
+];
 
-const crearTarea = (req, res)=>{
+const crearTarea = (req, res) => {
+    // console.log(req.body);
+    let tarea = req.body;
+    // la forma de capturar lo que me manda el usuario mediante el body es por su request (req)
+    tareas.push(tarea);
     return res.json({
         ok: true,
+        content: tareas,
         message: 'Se agregó la tarea exitosamente'
     })
-}  
+};
+const listarTareas = (req, res) => {
+    return res.json({
+        ok: true,
+        content: tareas,
+        message: null
+    })
+};
+
+const editarTareaPorId = (req, res) => {
+    // para capturar un valor pasado por la url se usa su metodo params que nos retorna un diccionario de todas las variables decladas en la ruta.
+    let { id_tarea } = req.params;
+    // validar que la posicion mandada exista, si existe hace el cambio de la tarea y si no, indicar que la tarea no existe.
+    console.log(tareas.length);
+    if (tareas.length >= id_tarea) {
+        //significa que la tarea existe
+        tareas[id_tarea - 1] = req.body;
+        return res.json({
+            ok: true,
+            message: 'Si existe la tarea',
+            content: tareas[id_tarea - 1]
+        })
+    } else {
+        // el id esta fuera de la longitud de mis tareas
+        return res.status(404).json({
+            ok: false,
+            content: null,
+            message: 'No existe esa tarea'
+        })
+    }
+}
+
+// eliminar una tarea segun su id
+const eliminarTareaPorId = (req, res)=>{
+    
+}
 
 module.exports = {
     // gracias a ES6 se interpreta como creaTarea: crearTarea, es decir la llave del json es igual al nombre de la variable a exportar
-    crearTarea, 
+    crearTarea,
+    listarTareas,
+    editarTareaPorId
 }
--- Semana 6/Tareas/src/routes/TareaRoutes.js
+++ Semana 6/Tareas/src/routes/TareaRoutes.js
@@ -1,12 +1,14 @@
 // se puede usar destructuracion para importar algo cuando queremos usar una parte de una libreria o archivo
 const { Router } = require('express');
 // importar el crearTarea por destructuracion
-const { crearTarea } = require('../controllers/TareaController');
+const { crearTarea, listarTareas, editarTareaPorId } = require('../controllers/TareaController');
 // const tareaController = require('../controllers/TareaController');
 
 const tarea_router = Router();
 
 tarea_router.post('/tarea', crearTarea );
+tarea_router.get('/tarea', listarTareas);
+tarea_router.put('/tarea/:id_tarea', editarTareaPorId);
 
 
 module.exports = tarea_router; // esta es una exportacion por defecto (export default tarea_router)
