c990bbd5854a650d62d7c36c25755944b74ce0fd
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-01-04 19:34:52 -0500

    avance

---
+++ Semana 6/Tareas/src/config/Server.js
@@ -0,0 +1,31 @@
+// npm i express
+const express = require('express');
+// npm i body-parser
+const bodyParser = require('body-parser');
+class Server {
+    constructor(){
+        // app es una instancia de mi clase express
+        this.app = express();
+        this.puerto = process.env.PORT || 5000;
+        this.configurarBodyParser();
+        this.rutas();
+    }
+    // el body parser es la forma en la cual el front me va a mandar informacion y yo tengo que definir que tipo de informacion voy a recibir (texto plano, json, xml...)
+    configurarBodyParser(){
+        this.app.use(bodyParser.json());
+    }
+    // aca voy a definir todas mis rutas raices de mi aplicacion para que cuando sean consultadas si no estan aquí automaticamente express retornará un 404 (not found)
+    rutas(){
+        // primero pongo la ruta (endpoint) y luego su comportamiento (que va a hacer cuando se llame a esa ruta)
+        this.app.get('/',(req, res)=>{
+            // toda la logica de esa ruta
+            res.status(200).send('La api funciona con éxito');
+        })
+    }
+    start(){
+        this.app.listen(this.puerto, ()=>{
+            console.log('El servidor esta corriendo exitosamente en el puerto',this.puerto);
+        });
+    }
+}
+module.exports = Server // export default ....
