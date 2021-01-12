cc2ba9de4a8283412743750daa4952e190d2362d
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-01-12 18:26:48 -0500

    conexion entre express y sequelize

--- Semana 7/Ecommerce/src/config/Server.js
+++ Semana 7/Ecommerce/src/config/Server.js
@@ -1,33 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { conexion } = require('./Sequelize');
+
 module.exports = class Server {
-    constructor (){
+    constructor() {
         this.app = express();
         this.puerto = process.env.PORT || 5000;
         this.CORS();
         this.configurarBodyParser();
         this.rutas();
     }
-    CORS(){
-        this.app.use((req,res,next)=>{
+    CORS() {
+        this.app.use((req, res, next) => {
             // el dominio que va a usar mi back (es el front)
-            res.header('Access-Control-Allow-Origin','*');
-            res.header('Access-Control-Allow-Headers','Content-Type, Authorization');
-            res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
             next();
         });
     }
-    configurarBodyParser(){
+    configurarBodyParser() {
         this.app.use(bodyParser.json());
     }
-    rutas(){
-        this.app.get('/', (req, res)=>{
+    rutas() {
+        this.app.get('/', (req, res) => {
             res.status(200).send('La api funciona correctamente 😎🍕');
         });
     }
-    start(){
-        this.app.listen(this.puerto, ()=>{
+    start() {
+        this.app.listen(this.puerto, () => {
             console.log(`Servidor corriendo exitosamente en el puerto ${this.puerto}`);
+            conexion.sync().then(()=>{
+                console.log('Base de datos sincronizada exitosamente')
+            }).catch((error)=>{
+                console.log(error);
+            })
         });
     }
 }
