9456184b84984fd3d20404bdf80e70d7797c4ee0
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2021-01-13 19:21:42 -0500

    devolver todas las categorias

--- Semana 7/Ecommerce/src/controllers/CategoriaController.js
+++ Semana 7/Ecommerce/src/controllers/CategoriaController.js
@@ -18,6 +18,36 @@ const createCategoria = async (req, res) => {
         });
     }
 }
+// Mientras arreglo mi micro amigos, otra vez MIL DISCULPAS por los inconvenientes, mientras soluciono este error, hagamos el devolverCategorias:
+// crear un controlador para devolver todas las Categorias en la base de datos y si es que no hay una categoria en la base de datos indicar que faltan crear con un status 404
+// implementar la ruta '/categoria' con el metodo correspondiente
+const devolverCategorias = (req, res)=>{
+    Categoria.findAll().then((categorias)=>{
+        if (categorias.length != 0){
+            return res.json({
+                ok: true,
+                content: categorias,
+                message: null
+            })
+        }else{
+            return res.status(404).json({
+                ok: false,
+                content: null,
+                message:'Falta crear las categorias'
+            })
+        }
+    }).catch((error)=>{
+        return res.status(500).json({
+            ok: false,
+            content: error,
+            message: 'Hubo un error al devolver las categorias'
+        })
+    })
+    // segundo metodo, va con async obligatoriamente
+    // let categorias = await Categoria.findAll()
+
+}
 module.exports = {
-    createCategoria
+    createCategoria,
+    devolverCategorias
 }
--- Semana 7/Ecommerce/src/routes/CategoriaRoutes.js
+++ Semana 7/Ecommerce/src/routes/CategoriaRoutes.js
@@ -3,5 +3,6 @@ const CategoriaController = require('../controllers/CategoriaController');
 const categoria_router = Router();
 
 categoria_router.post('/categoria', CategoriaController.createCategoria);
+categoria_router.get('/categoria', CategoriaController.devolverCategorias);
 
 module.exports = categoria_router;
