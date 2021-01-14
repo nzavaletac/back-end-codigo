48c0ebc5c6f1ef5a41c0094df2531c222477dba0
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2021-01-13 20:22:09 -0500

    creacion almacen finalizada

--- Semana 7/Ecommerce/src/controllers/AlmacenController.js
+++ Semana 7/Ecommerce/src/controllers/AlmacenController.js
@@ -1,14 +1,11 @@
 const { Almacen } = require("../config/Sequelize");
 
 const createAlmacen = (req, res) => {
-  Almacen.build(req.body)
-    .then((objAlmacen) => {
-      // anidamiento de promesas
-      console.log("1");
-      return Almacen.save(objAlmacen);
-    })
+  // si usamos el build eso no retorna una promesa, eso retorna un objeto tipo Almacen, no retorna una promesa porque no hace la consulta a la bd, el build sirve para construir el tipo de dato mas no lo verifica con los datos en la bd
+  let objAlmacen = Almacen.build(req.body);
+  // el metodo save va de la mano con el build y solo se ejecutará luego que tengamos una instancia de la clase Almacen creada y este SI retornará una promesa puesto que ya esta haciendo peticion a la base de datos
+  objAlmacen.save()
     .then((almacenCreado) => {
-      console.log("2");
       return res.status(201).json({
         ok: true,
         content: almacenCreado,
