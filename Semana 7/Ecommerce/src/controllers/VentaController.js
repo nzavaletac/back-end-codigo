25d932197d3a2f147c932a80a11cf569e687edfd
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-01-19 20:51:25 -0500

    transaccion creada

--- Semana 7/Ecommerce/src/controllers/VentaController.js
+++ Semana 7/Ecommerce/src/controllers/VentaController.js
@@ -1,50 +1,68 @@
-const {CabeceraVenta, DetalleVenta, Usuario, Producto} = require("../config/Sequelize");
+const {
+  CabeceraVenta,
+  DetalleVenta,
+  Usuario,
+  Producto,
+  Categoria,
+  conexion,
+} = require("../config/Sequelize");
 
-const crearVenta = async(req, res)=>{
-    let {fecha, total, igv, usuario, productos} = req.body;
-    // Pasos a realizar:
-    // Validar que exista el usuario y que el producto exista
-    let usuarioEncontrado = await Usuario.findByPk(usuario);
-    let productoEncontrado = true;
-    for (const key in productos) {
-        // Validar que el producto tenga la categoria activa, usar include 
-        let producto = await Producto.findOne({
-            where:{
-                productoId:productos[key].producto
-            },
-            //...
-        });
-        if(producto === null){
-            productoEncontrado = false;
-            // el break hace que el bucle, switch o condicional termine
-            break;
-            // el return hace devolver una respuesta a la funcion , en este caso al controlador
-        }
-    }
-    if (productoEncontrado === false || usuarioEncontrado === null){
-        return res.status(400).json({
-            ok :false,
-            message: 'Producto o Usuario incorrectos intente nuevamente',
-            content: null
-        })
-    }
-    return res.json({
-        ok:true
+const crearVenta = async (req, res) => {
+  const miTransaccion = await conexion.transaction();
+  let { fecha, total, igv, usuario, productos } = req.body;
+  // Pasos a realizar:
+  // Validar que exista el usuario y que el producto exista
+  let usuarioEncontrado = await Usuario.findByPk(usuario);
+  let productoEncontrado = true;
+  for (const key in productos) {
+    // Validar que el producto tenga la categoria activa, usar include
+
+    let producto = await Producto.findOne({
+      where: {
+        productoId: productos[key].producto,
+      },
+      include: {
+        model: Categoria,
+        where: {
+          categoriaEstado: true,
+        },
+      },
     });
-    
-    // 1. Revisar si los productos cuentan con la cantidad suficiente
-    // 2. Crear la cabecera
-    // 3. Crear el detalle
-    // 4. Sacar los calculos
-    // 5. Restar el inventario
-    // 7. Borrar el contenido del carrito de ese usuario
-    // 6. Devolver el resultado
-    
-}
+    let cantidad_actual = producto.productoCantidad; // lo que yo tengo
+    let cantidad_solicitada = productos[key].cantidad; // lo que me piden
 
-const mostrarVentasDelUsuario = (req, res)=>{}
+    // 1. Revisar si los productos cuentan con la cantidad suficiente
+    if (producto === null || cantidad_actual < cantidad_solicitada) {
+      productoEncontrado = false;
+      // el break hace que el bucle, switch o condicional termine
+      break;
+      // el return hace devolver una respuesta a la funcion , en este caso al controlador
+    }
+  }
+  if (productoEncontrado === false || usuarioEncontrado === null) {
+    return res.status(400).json({
+      ok: false,
+      message: "Producto o Usuario incorrectos intente nuevamente",
+      content: null,
+    });
+  }
+  
+  // 2. Crear la cabecera
+  let cabeceraCreada = await CabeceraVenta.create({
+      cabeceraVentaFecha: fecha,
+      cabeceraVentaTotal: total,
+      cabeceraVentaIGV: igv
+  },{transaction: miTransaccion});
+  // 3. Crear el detalle
+  // 4. Sacar los calculos
+  // 5. Restar el inventario
+  // 7. Borrar el contenido del carrito de ese usuario
+  // 6. Devolver el resultado
+};
+
+const mostrarVentasDelUsuario = (req, res) => {};
 
 module.exports = {
-    crearVenta,
-    mostrarVentasDelUsuario
-}
+  crearVenta,
+  mostrarVentasDelUsuario,
+};
