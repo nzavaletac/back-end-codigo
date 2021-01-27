5085230a772690c08efd77ec6e2c1cb19cfc2462
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-01-26 20:51:06 -0500

    avance de muchos a muchos

--- Semana 8/plataforma_educativa/src/controllers/usuario.js
+++ Semana 8/plataforma_educativa/src/controllers/usuario.js
@@ -1,4 +1,4 @@
-const { Usuario } = require("../config/mongoose");
+const { Usuario, Curso } = require("../config/mongoose");
 
 const crearUsuario = async (req, res) => {
   try {
@@ -51,16 +51,47 @@ const login= async (req, res)=>{
   }
 };
 
-const inscribirUsuarioCurso = (req, res)=>{
+const inscribirUsuarioCurso = async(req, res)=>{
   /*
-  1. usar JWT para ver que usuario esta queriendo acceder a que curso
-  2. mediante la url indicar el id del curso 
-  3. ver si el usuario ya esta inscrito en el curso y si lo esta indicar que ya no se puede enrolar en el curso
+  1. usar JWT para ver que usuario esta queriendo acceder a que curso ✔
+  2. mediante la url indicar el id del curso ✔
+  2.1 Ver si el curso existe ✔
+  3. ver si el usuario ya esta inscrito en el curso y si lo esta indicar que ya no se puede enrolar en el curso, se puede realizar en dos formas, la primera es ver dentro del cursoEncontrado sus usuarios, la segunda forma era traer el usuario encontrado segun su id y ver sus cursos
   4. si no esta inscrito enrolarlo en el curso y responder que se registro exitosamente
   */
-  return res.send('ok');
+  let {id_curso} = req.params;
+  let {usuario_id} = req.usuario;
+  console.log(req.usuario);
+  try {
+    let cursoEncontrado = await Curso.findById(id_curso);
+    let usuarioEncontrado = await Usuario.findById(usuario_id);
+    // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/includes
+    // Primera forma paso 3
+    // let resultado = cursoEncontrado.usuarios.includes(usuario_id);
+    // console.log(cursoEncontrado.usuarios.includes(usuario_id));
 
-}
+    // Segunda Forma paso 3
+    let resultado = usuarioEncontrado.cursos.includes(id_curso);
+    console.log(usuarioEncontrado.cursos.includes(id_curso));
+
+    if (resultado){ // resultado === true
+      return res.status(401).json({
+        ok: false,
+        content: null,
+        message:'Usuario ya se encuentra enrolado en el curso'
+      })
+    }
+
+    return res.send('ok');
+  } catch (error) {
+    return res.status(401).json({
+      ok:false,
+      content: error,
+      message:'Id de curso incorrecto'
+    })    
+  }
+};
+
 const mostrarCursosUsuario = (req, res)=>{
 
 }
