e533d36dcf661f033e9a5449c8dd479e37438ef5
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-02-01 19:31:50 -0500

    ejercicio

--- Semana 9/Votaciones/src/controllers/administrador.js
+++ Semana 9/Votaciones/src/controllers/administrador.js
@@ -1,40 +1,70 @@
-const {Voto, VotoCongresal, Partido} = require("../config/sequelize");
-const {Sequelize} = require("sequelize");
+const { Voto, VotoCongresal, Partido } = require("../config/sequelize");
+const { Sequelize } = require("sequelize");
 
-const resultadosPresidenciales = async (req,res)=>{
-    // el metodo count se encarga de contar todos los registros de la tabla
-    // SELECT COUNT(*) FROM T_VOTO WHERE PARTIDO_ID=2;
-    // console.log(await Voto.count({
-    //     where:{
-    //         partido_id:2
-    //     }
-    // }))
-    // SELECT PARTIDO_ID,COUNT(*) FROM T_VOTO GROUP BY PARTIDO_ID;
-    const votos = await Voto.count({
-        attributes:['partido_id'],
-        group:['partido_id'],
-    });
-    // Otra forma de hacer el conteo es usar las funciones internas de sequelize (Sequelize.fn())
-    const votos2 = await Voto.findAll({
-        attributes : [[Sequelize.fn('COUNT', Sequelize.col('votos.partido_id')),'numero_votos']],
-        group: ['votos.partido_id'],
-        include:{
-            model: Partido,
-            as: 'partidos'
-        }
-    })
-    console.log(votos);
-    return res.json({
-        ok:true,
-        content: votos,
-        content2: votos2,
-        message: null
-    })
-}
+const resultadosPresidenciales = async (req, res) => {
+  // el metodo count se encarga de contar todos los registros de la tabla
+  // SELECT COUNT(*) FROM T_VOTO WHERE PARTIDO_ID=2;
+  // console.log(await Voto.count({
+  //     where:{
+  //         partido_id:2
+  //     }
+  // }))
+  // SELECT PARTIDO_ID,COUNT(*) FROM T_VOTO GROUP BY PARTIDO_ID;
+  const votos = await Voto.count({
+    attributes: ["partido_id"],
+    group: ["partido_id"],
+  });
+  // Otra forma de hacer el conteo es usar las funciones internas de sequelize (Sequelize.fn())
+  const votos2 = await Voto.findAll({
+    attributes: [
+      [
+        Sequelize.fn("COUNT", Sequelize.col("votos.partido_id")),
+        "numero_votos",
+      ],
+    ],
+    group: ["votos.partido_id"],
+    include: {
+      model: Partido,
+      as: "partidos",
+    },
+  });
+  console.log(votos);
+  return res.json({
+    ok: true,
+    content: votos,
+    content2: votos2,
+    message: null,
+  });
+};
 
-const resultadosCongresalesPaginados = (req,res)=>{}
+const resultadosCongresalesPaginados = (req, res) => {
+  // Devolver todos los resultados (count) de los congresistas en el cual, además, se incluya el nombre del congresista y tambien su nombre del partido
+  /**
+   * {
+   *  ok:true,
+   *  content:{
+   *     numero_votos:10,
+   *     congresista:{
+   *          congresista_nombre: "juan martinez",
+   *          congresista_numero: 10,
+   *          partido:{
+   *              partido_nombre: "La naranja mecanica"
+   *          }
+   *     },
+   *     numero_votos:15,
+   *     congresista:{
+   *          congresista_nombre: "maria rodriguez",
+   *          congresista_numero: 7,
+   *          partido:{
+   *              partido_nombre: "La naranja mecanica"
+   *          }
+   *     }
+   *  }
+   * }
+   */
+};
 
 module.exports = {
-    resultadosPresidenciales,
-    resultadosCongresalesPaginados
-}
+  resultadosPresidenciales,
+  resultadosCongresalesPaginados,
+};
