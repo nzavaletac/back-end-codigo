a450334842db0c0bacc68c7107ab490967a3be4c
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-02-11 21:15:53 -0500

    avance sockets

--- Semana 11/SocketioExpress/src/config/server.js
+++ Semana 11/SocketioExpress/src/config/server.js
@@ -15,16 +15,35 @@ module.exports = class ServerSocket {
     misSockets(){
         // El metodo ON me sirve para recibir una llamada del client
         // en los sockets existen metodos preestablecidos que solamente podemos interactuar con ellos y estos son : connect, disconnect
+        let usuarios = [];
+        let mensajes = [];
         this.socket.on('connect', (cliente)=>{
             console.log(cliente.id);
+            // proximamente salas....
+            // console.log(cliente.rooms);
+            // cliente.join('room1');
+            // console.log(cliente.rooms);
+            cliente.on('registrar',(username)=>{
+                const objUsuario = {
+                    id: cliente.id,
+                    username: username
+                }
+                usuarios.push(objUsuario);
+                // el metodo emit solamente sirve para retornarnos la emision al mismo usuario
+                cliente.emit('lista-usuarios', usuarios);
+                // si quieremos notificar a todos los clientes conectados deberemos utilizar un broadcast pero no se notificará al cliente actual
+                cliente.broadcast.emit('lista-usuarios', usuarios);
+
+            })
+            
+            cliente.on('mensaje-nuevo', (mensaje)=>{
+                console.log(mensaje);
+                cliente.emit('enviar-mensajes',['hola','hola amigo','vamos a montar bici?'])
+            });
             cliente.on('disconnect',(reason)=>{
                 console.log(reason);
                 console.log('Se desconecto! :(');
             });
-            cliente.on('mensaje-nuevo', (mensaje)=>{
-                console.log(mensaje);
-                cliente.emit('enviar-mensajes',['hola','hola amigo','vamos a montar bici?'])
-            })
         })
     }
 
